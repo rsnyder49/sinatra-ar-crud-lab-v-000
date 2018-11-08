00943b4472fb1f02494b59ab2577c6c1e9e62f4f
Robert Snyder <r.snyder49@gmail.com>
2018-11-08 22:34:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,45 +16,39 @@ class ApplicationController < Sinatra::Base
     erb :new 
   end 
   
-  post '/posts' do
-    @post = Post.new({name: params[:name], content: params[:content]})
-    @post.save
-    
+post '/posts' do
+    @post = Post.create(params)
     redirect to '/posts'
   end
   
   get '/posts' do
     @posts = Post.all
-     
     erb :index
   end
   
+  get '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+  
   get '/posts/:id/edit' do
-    @post = Post.find(params[:id])
-    
-     erb :edit
+    @post = Post.find_by_id(params[:id])
+    erb :edit
   end
   
   patch '/posts/:id' do
-    @post = Post.find(params[:id])
-    @post.update(name: params[:name])
-    @post.update(content: params[:content])
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
     @post.save
-    
     erb :show
   end
   
-  get '/posts/:id' do
-    @post = Post.find(params[:id])
-     
-    erb :show
-  end
-     
   delete '/posts/:id/delete' do
-    @post = Post.find(params[:id])
-    @post.destroy
-     
+    @post = Post.find_by_id(params[:id])
+    @post.delete
     erb :delete
   end
+
   
 end
